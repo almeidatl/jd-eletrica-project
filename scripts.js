35dfeffb8e7a581b148182c933af05e93e041dc5
Thalisson Almeida <134632200+almeidatl@users.noreply.github.com>
2024-01-22 19:36:04 -0300

    css update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
 //function to open mobile menu
 
+const menuScreen = document.querySelector('.menuScreen')
 const menuBtn = document.querySelector('.menuMobileMenu')
+
+function openMenu(){
+    menuScreen.classList.remove('menuScreen')
+    menuScreen.classList.add('menuScreenActive')
+
+}
